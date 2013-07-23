34eb0f8507b0350fed2fb441da926f5a926973df
Assim <assim1991@gmail.com>
2013-07-23 14:40:58 +0400

    Added ActionBar tabs.

--- src/com/masaaroman/eessmobile/MainActivity.java
+++ src/com/masaaroman/eessmobile/MainActivity.java
@@ -1,23 +1,57 @@
 package com.masaaroman.eessmobile;
 
 import android.os.Bundle;
+import android.app.ActionBar;
+import android.app.ActionBar.TabListener;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements TabListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        
+        final ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        
+        actionBar.addTab(actionBar.newTab().setText("Departments")
+                .setTabListener(this));
+        actionBar.addTab(actionBar.newTab().setText("Search")
+                .setTabListener(this));
+        actionBar.addTab(actionBar.newTab().setText("Cart")
+                .setTabListener(this));
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab,
+        FragmentTransaction fragmentTransaction) {
+      
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab,
+        FragmentTransaction fragmentTransaction) {
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab,
+        FragmentTransaction fragmentTransaction) {
     }
     
 }
