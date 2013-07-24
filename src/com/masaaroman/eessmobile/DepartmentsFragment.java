12748a602592d90b8eb3f9cfd1acf9355a58500b
Assim Al-Marhuby <assim1991@gmail.com>
2013-07-24 18:56:25 +0400

    Trying out to manipulate views for the fragments.

--- src/com/masaaroman/eessmobile/DepartmentsFragment.java
+++ src/com/masaaroman/eessmobile/DepartmentsFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class DepartmentsFragment extends Fragment {
 
@@ -12,6 +13,10 @@ public class DepartmentsFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View myFragmentView = inflater.inflate(R.layout.fragment_departments, container, false);
+		
+		TextView tv = (TextView)myFragmentView.findViewById(R.id.depttext);
+		tv.setText("This is the new text");
+		
 		return myFragmentView;
 	}
 
